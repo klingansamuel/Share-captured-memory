e4b20460ecdd51ffd455624ba5e97999b157f10e
klingansamuel <klingansamuel@gmail.com>
2024-09-24 17:31:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import HomePage from "./containers/Homepage";
+// import HomePage from "./containers/Homepage";
 
 function App() {
+  const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
 
 export default App;
+{
+  /* <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+      </Routes>
+    </BrowserRouter> */
+}
